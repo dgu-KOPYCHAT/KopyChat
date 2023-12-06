b8dc53e00030640ca07167f0c53c2c051cb7fd0a
Birdcantfly <71496474+ner-di-dream@users.noreply.github.com>
2023-12-06 16:15:38 +0900

    보드css

---
+++ reactweb/frontend/src/pages/BoardCSS.js
@@ -0,0 +1,10 @@
+import styled, { css } from "styled-components";
+
+export const Entire = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-color: #222233;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;

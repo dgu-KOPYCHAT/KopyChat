7bbaa5588e81bf5079ac5cdd64612faf8103e09e
brladder77_mac <braldder77@naver.com>
2023-12-12 10:44:10 +0900

    modify timestamp in board

--- reactweb/frontend/src/pages/BoardPage.js
+++ reactweb/frontend/src/pages/BoardPage.js
@@ -10,39 +10,43 @@ import DifficultyIcon from "../components/DifficultyIcon.js";
 import axios from "axios";
 
 const BoardPage = () => {
-	const [posts, setPosts] = useState([]);
-	useEffect(() => {
-		const fetchPosts = async () => {
-			try {
-				axios.get("http://localhost:8000/board/get").then((res) => {
-					setPosts(res.data);
-				});
-			} catch (error) {
-				console.error("Error fetching data:", error);
-			}
-		};
-		fetchPosts();
-	}, []);
-	function QuizList() {
-		return posts.map((item, index) => (
-			<PostCard
-				id={item.id}
-				name={item.title}
-				writer={item.nickname}
-				createdDate={item.createdAt}
-			/>
-		));
-	}
+    const [posts, setPosts] = useState([]);
 
-	const timeToString = () =>
-		new Intl.DateTimeFormat("ko-KR", {
-			hour: "numeric", // 시간은 숫자로
-			minute: "numeric", // 분도 숫자로
-			day: "numeric", // 날도 숫자로
-			month: "long", // 달은 글자로
-			year: "numeric", // 연도는 숫자로
-			weekday: "long", // 요일은 글자로
-		}).format();
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/board/get");
+                setPosts(response.data);
+            } catch(error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchPosts();
+    }, []);
+
+    const timeToString = (date) => {
+        return new Intl.DateTimeFormat('ko-KR', {
+            hour: 'numeric', // 시간은 숫자로
+            minute: 'numeric', // 분도 숫자로 (Corrected from 'minutes' to 'minute')
+            day: 'numeric', // 날도 숫자로
+            month: 'long', // 달은 글자로
+            year: 'numeric', // 연도는 숫자로
+            weekday: 'long', // 요일은 글자로
+        }).format(new Date(date));
+    };
+
+    const QuizList = () => {
+        return posts.map((item, index) => (
+            <PostCard 
+                key={item.id} // Added key for React list rendering
+                id={item.id} 
+                name={item.title} 
+                writer={item.nickname} 
+                createdDate={timeToString(item.createdAt)}
+            />
+        ));
+    };
+
 
 	return (
 		<div>
